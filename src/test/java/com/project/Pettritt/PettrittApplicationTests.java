5602c62b1c3263cb4ccc24480e18f2e7f2e3fd84
yuhyun <jadejung95@gmail.com>
2022-06-20 20:04:11 +0900

    first

---
+++ src/test/java/com/project/Pettritt/PettrittApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Pettritt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettrittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
