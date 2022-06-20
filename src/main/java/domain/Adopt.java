c323cf03acb98585494e8131f0f8307d4d9b2db7
yuhyun <jadejung95@gmail.com>
2022-06-20 21:53:03 +0900

    dependency 추가

--- src/main/java/domain/Adopt.java
+++ src/main/java/domain/Adopt.java
@@ -1,5 +1,21 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "adoptboard")
+@Setter
+@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@RequiredArgsConstructor
 public class Adopt {
 
 }
